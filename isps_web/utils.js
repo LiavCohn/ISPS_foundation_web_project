443dbd0c3c9478698f04867f2eac8083d758e9da
LiavCohn <liavcohen955@gmail.com>
2021-08-18 10:40:43 +0300

    Add project files.

---
+++ isps_web/utils.js
@@ -0,0 +1,9 @@
+﻿//for the manu bar
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
